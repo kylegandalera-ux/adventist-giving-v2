d27b90904af39e1fb5dcc64f63622e92b8c94c9f
kylegandalera-ux <kylegandalera@adventist.ph>
2026-05-17 20:22:43 +0800

    Delete docs/build-a-vercel-ready-donation-web directory

--- docs/build-a-vercel-ready-donation-web/app/awr-89-1-fm/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function AwrFrequencyAliasPage() {
-  redirect("/awr-891-fm");
-}
--- docs/build-a-vercel-ready-donation-web/app/hope-channel/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function HopeChannelAliasPage() {
-  redirect("/hope-channel-philippines");
-}
